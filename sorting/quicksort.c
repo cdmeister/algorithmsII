d4a860820867d5bfab995a5a2e8ab8933e8144a6
Moeiz Riaz <moeizr@umich.edu>
2019-08-03 17:47:52 -0700

    adding 3way of quicksort

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -23,6 +23,49 @@ void exchange(int list[], int a, int b,int * num_counter){
 
 }
 
+int partition3way(int list[], int left, int right){
+
+  int pivotIndex= left +(right - left)/2;
+  int pivot = list[pivotIndex];
+  printf("pivot: %d pivotIndex: %d left: %d right: %d\n",pivot,pivotIndex,left,right);
+  int i = left, lt=left,gt=right;
+  while(i <= gt){
+    int temp = list[i];
+    printf("temp: %d\n",temp);
+    if(temp < pivot){
+      printf("temp: %d\ti: %d\tless than\n",temp,i);
+      exchange(list,i,lt,&num_swaps2);
+      print_array(list,16);
+      lt++;
+      i++;
+    }
+    else if(pivot == temp){
+      printf("temp: %d\ti: %d\tequal than\n",temp,i);
+      i++;
+    }
+    else if(temp > pivot){
+      printf("temp: %d\ti: %d\tgreater than\n",temp,i);
+      exchange(list,i,gt,&num_swaps2);
+     print_array(list,16);
+      --gt;
+    }
+    printf("lt: %d\ti: %d\tgt:%d\n",lt,i,gt);
+  }
+  return lt;
+}
+
+
+void quicksort3way(int list[],int left,int right){
+
+  if(left < right){
+    int pivot = partition3way(list,left,right);
+    quicksort3way(list,left,pivot);
+    quicksort3way(list,pivot+1,right);
+  }
+
+}
+
+
 int partition(int list[], int left, int right){
 
   int pivotIndex= left +(right - left)/2;
@@ -67,6 +110,13 @@ int main(void){
   print_array(list,size);
   quicksort(list,0,size-1);
   print_array(list,size);
+  printf("Second Quicksort\n");
+  int list2[] = {9,5,12,6,3,8,15,1,4,2,7,10,11,13,14,0};
+  print_array(list2,size);
+  quicksort3way(list2,0,size-1);
+  print_array(list2,size);
+
   printf("Num swaps for quicksort: %d\n",num_swaps1);
+  printf("Num swaps for quicksort3way: %d\n",num_swaps2);
   return 0;
 }
