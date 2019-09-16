0adcfb54df3140a81bb2b65fc8a0985b9734fa24
Moeiz Riaz <moeizr@umich.edu>
2019-09-15 21:36:33 -0700

    adding basic implimentation of linked list symbol table

--- data_struct/symbol_table.c
+++ data_struct/symbol_table.c
@@ -4,18 +4,122 @@ struct symbol_table * ST(){
 
   struct symbol_table * temp = malloc(sizeof(struct symbol_table));
   temp->size=0;
-  memset(temp->list,0,MAX_SIZE*sizeof(struct node *));
+  temp->head= NULL;
+  temp->tail= NULL;
   return temp;
 }
 
-void put( int key, char * value){
+void put(struct symbol_table * st, int key, char * value){
 
+  struct node * it_node = st->head;
+  while(it_node != NULL){
+    if(key == it_node->key){
+      printf("match %d\n",key);
+      it_node->value = (void *) value;
+      return;
+    }
+    it_node=it_node->next;
+  }
+  struct node * new_node = (struct node *) malloc(sizeof(struct node));
+  new_node->key = key;
+  new_node->value = (void *) value;
+  // If you have empty linked list only for the first node
+  // Set the head ptr to the first node
+  if(st->head == NULL){
+    st->head = new_node;
+    st->tail = new_node;
+    new_node->next = NULL;
+    new_node->prev = st->head;
+  }
+  else{
+    struct node * prev_node = st->tail;
+    prev_node->next = new_node;
+    new_node->next = NULL;
+    new_node->prev = prev_node;
+    st->tail = new_node;
+  }
+  st->size++;
+}
 
+char * get(struct symbol_table * st,int key){
 
+  struct node * it_node = st->head;
+  while (it_node != NULL){
+    if(it_node->key == key) return (char *) it_node->value;
+    it_node = it_node->next;
+  }
+  return "ERROR";
+}
 
+int size(struct symbol_table * st){
+  return st->size;
+}
+
+void delete(struct symbol_table * st, int key){
+  // 3 cases:
+  //  1. Key is the head of the linked list
+  //  2. Key is the tail of the linked list
+  //  3. Key is neither of the two
+  struct node * it_node = st->head;
+  while (it_node != NULL){
+    if(it_node->key == key){
+      if(it_node == st->head){
+        st->head = st->head->next;
+      }
+      else if(it_node == st->tail){
+        st->tail = st->tail->prev;
+      }
+      else{
+        struct node * prev_node = it_node->prev;
+        struct node * next_node = it_node->next;
+        prev_node->next = next_node;
+        next_node->prev = prev_node;
+      }
+      free(it_node);
+      st->size--;
+      return;
+    }
+    it_node= it_node->next;
+  }
+  return;
+}
+
+void * keys(struct symbol_table * st){
+
+  int * key_it = malloc(st->size*sizeof(int));
+  struct node * it_node = st->head;
+  int i;
+  for(i=0;i<st->size;i++){
+    key_it[i]=it_node->key;
+    it_node=it_node->next;
+  }
+  return (void *) key_it;
+}
+
+void printLL(struct symbol_table * st){
+  struct node * it_node = st->head;
+  while(it_node != NULL){
+    printf("Key: %d\tValue: %s\n",it_node->key,(char *)it_node->value);
+    it_node = it_node->next;
+  }
+  printf("size: %llu\n",st->size);
 }
 
 int main(void){
+  struct symbol_table * test = ST();
+  put(test,3,"YOLO");
+  put(test,4,"POOO");
+  put(test,5,"FOO");
+  printLL(test);
+  int * key =(int *) keys(test);
+  int i;
+  for(i=0;i<test->size;i++){
+    printf("Keys: %d\t",key[i]);
+  }
+  free(key);
+  printf("\n");
+  delete(test,4);
+  printLL(test);
   return 0;
 }
 
--- data_struct/symbol_table.h
+++ data_struct/symbol_table.h
@@ -2,9 +2,9 @@
 #include "string.h"
 #include "stdlib.h"
 
-#define MAX_SIZE 10
 #define MAX_NUM_CHAR 15
 
+// Sequential Symbol Table using linked list
 // No duplicates
 // New value replaces old value of same key
 // No key assosiated with NULL
@@ -13,11 +13,13 @@ struct node {
   int key;
   void * value;
   struct node * next;
+  struct node * prev;
 };
 
 struct symbol_table {
   unsigned long long size;
-  struct node * list[MAX_SIZE];
+  struct node * head; // Head ptr
+  struct node * tail; // Head ptr
 
 };
 
@@ -26,14 +28,14 @@ struct symbol_table * ST();
 
 // put key-value pair into the table
 // ( remove key from table if value is null)
-void put(int key, char * value);
+void put(struct symbol_table * st,int key, char * value);
 
 // Value paired with key
 // (null if key is absent)
-char * get(int key);
+char * get(struct symbol_table * st, int key);
 
 // remove key(and its value) from table
-void delete(int key);
+void delete(struct symbol_table *st, int key);
 
 // is there a value paired with key?
 int contains(int key);
@@ -44,7 +46,12 @@ int isEmpty(struct symbol_table * st);
 // number of key-value pairs in the table
 int size(struct symbol_table * st);
 
-// All the keys in the table
-char * keys(struct symbol_table * st);
+// Idea is to return a iterator that will allow one to iterate through all keys
+// but we will return a dynamically allocated array of all keys and
+// programmer will have to free memory when done.
+void * keys(struct symbol_table * st);
+
+// Print Link List
+void printLL(struct symbol_table * st);
 
 struct node * createNode();
